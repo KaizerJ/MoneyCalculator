28b33b5b320b49e38547fad139b05ea53b32b137
KaizerJ <>
2019-10-27 23:09:53 +0000

    It now loads the exchange rates between from and to every currency.

--- src/implPersistance/RestExchangeRateLoader.java
+++ src/implPersistance/RestExchangeRateLoader.java
@@ -4,45 +4,51 @@ import model.Currency;
 import model.ExchangeRate;
 import persistance.ExchangeRateLoader;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.Scanner;
 import com.google.gson.*;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class RestExchangeRateLoader implements ExchangeRateLoader {
 
     @Override
     public List<ExchangeRate> load(List<Currency> currencies) { 
-        String exchangeJson = null;
-        try {
-            URL url = new URL("https://api.exchangeratesapi.io/latest");
-            Scanner scanner = new Scanner(url.openStream());
-            exchangeJson = scanner.nextLine();
-        } 
-        catch (IOException ignored) {}
         
         Map<String, Currency> currencyMap = new HashMap<>();
         for (Currency currency : currencies) {
             currencyMap.put(currency.getCode(), currency);
         }
         
+        String exchangeJson;
+        URL url;
+        LocalDate date;
+        Map rates;
+        Currency base;
         Gson gson = new Gson();
-        Map rates = gson.fromJson(exchangeJson, HashMap.class);
-        
-        LocalDate date = LocalDate.parse((CharSequence) rates.get("date"));
-        Currency base = currencyMap.get(rates.get("base"));
-        rates = (Map) rates.get("rates");
-        
         ArrayList<ExchangeRate> exchangeRates = new ArrayList<>();
-        for (Object code : rates.keySet()) {
-            exchangeRates.add(new ExchangeRate(date, (double) rates.get(code), base, currencyMap.get(code)));
+        for (Currency currency : currencies) {            
+            exchangeJson = null;
+            try {
+                url = new URL("https://api.exchangeratesapi.io/latest?base=" + 
+                                    currency.getCode());
+                Scanner scanner = new Scanner(url.openStream());
+                exchangeJson = scanner.nextLine();
+            } 
+            catch (IOException ignored) {}
+
+            rates = gson.fromJson(exchangeJson, HashMap.class);
+
+            date = LocalDate.parse((CharSequence) rates.get("date"));
+            base = currencyMap.get(rates.get("base"));
+            rates = (Map) rates.get("rates");
+
+            for (Object code : rates.keySet()) {
+                exchangeRates.add(new ExchangeRate(date, (double) rates.get(code), base, currencyMap.get(code)));
+            }
         }
                 
         return exchangeRates;
